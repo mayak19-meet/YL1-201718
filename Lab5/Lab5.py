21e13ffdcbb821d8fe7c6ea51f8ce45b56d36462
mayak19-meet <mayak19@meet.mit.edu>
2017-11-27 18:39:52 +0200

    Update Lab5.py

--- Lab5/Lab5.py
+++ Lab5/Lab5.py
@@ -16,5 +16,30 @@ Square1=Square(30)
 Square.random_color()
 
 		
+class Hexagon (turtle):
+	def __init__(self, size,shape):
+		Turtle.__init__(self)
+		self.shapesize(size)
+		turtle.shape("Hexagon",(50,50),(70,50),(90,25),(70,0),(50,0),(30,25))
+class color(Hexagon):
+	def __init__(self, color):
+		turtle(color, self).__init__()
+		self.color = color
 
+class speed(Hexagon):
+	def __init__(self, speed):
+		turtle(speed, self).__init__()
+		self.speed = speed
+
+class speed(Hexagon):
+	def __init__(self, speed):
+		turtle(speed, self).__init__()
+		self.speed = speed
+
+
+
+		
+
+
+			
 			
